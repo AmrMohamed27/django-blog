cabfc45bc46e856d9be6d6737ecf4304713c53f5
AmrMohamed27 <amrmohamedorigi44@gmail.com>
2025-01-20 00:34:04 +0200

    feat: add password reset by email

--- django_project/urls.py
+++ django_project/urls.py
@@ -6,12 +6,42 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
-    path('logout/', user_views.logout_view, name="logout"),
-    path('profile/', user_views.profile, name='profile'),
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("register/", user_views.register, name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="users/login.html", redirect_authenticated_user=True
+        ),
+        name="login",
+    ),
+    path("logout/", user_views.logout_view, name="logout"),
+    path("profile/", user_views.profile, name="profile"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="users/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
 # Add static media file's root url for development
 if settings.DEBUG:
